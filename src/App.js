c83daf0dceb9f6ecdf33d55c829d39ff08cf1e7d
yaswanth1201 <yaswanthk1201@gmail.com>
2024-12-31 20:54:51 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Components/Login'
+import Signup from './Components/Signup'
+import Dashboard from './Components/Dashboard';
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Movies from './Components/Pages/Movies';
+import Moviedetails from './Components/Details/Moviedetails';
+import Webseries from './Components/Pages/Webseries';
+import Webdetails from './Components/Details/Webdetails';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:Id' element={<Moviedetails/>}/>
+        <Route path='/webseries' element={<Webseries/>}/>
+        <Route path='/webseries/:Id' element={<Webdetails/>}/>
+    
+
+    
+        
+
+      </Routes>
+    </Router>
+  
+  )
 }
 
-export default App;
+export default App
+
+
